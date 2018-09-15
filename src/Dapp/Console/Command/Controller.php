69075d412fbda38305b9e081f593637c6f6f2c9e
Edward Teach <edward@edwardteach.co>
2018-09-15 01:13:48 -0700

    updated controller creation

--- src/Dapp/Console/Command/Controller.php
+++ src/Dapp/Console/Command/Controller.php
@@ -20,46 +20,71 @@ class Controller extends Command
     protected function configure()
     {
         $this->setName('controller')
-            ->setDescription('Create a new Dappur model')
-            ->addArgument('controller-name', InputArgument::REQUIRED, 'What table would you like to create a model for?')
+            ->setDescription('Create a new Controller')
+            ->addArgument('name', InputArgument::REQUIRED, 'What table would you like to create a model for?')
             ->setHelp('Creates a new Dappur model');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
-        $controllerName = $input->getArgument('controller-name');
-
-        $dappur = CliUtils::isDappur();
-
-        if (!CliUtils::isCamelCase($controllerName)) {
-            throw new \InvalidArgumentException('Controller name needs to be CamelCaseFormat.');
+        $controllerName = $input->getArgument('name');
+        $separated = explode("\\", $controllerName);
+        foreach ($separated as $value) {
+            if (!CliUtils::isCamelCase($value)) {
+                throw new \InvalidArgumentException('Controller names needs to be CamelCaseFormat.');
+            }
         }
 
-        if ($dappur) {
+        if (CliUtils::isDappur()) {
             $containerBootstrap = getcwd() . '/app/bootstrap/controllers.php';
             $controller = getcwd() . '/app/src/Controller/'.$controllerName.'.php';
+            $base = getcwd() . '/app/src/Controller/';
+            $namespace = 'Dappur\Controller';
+            $controllerFileName = "";
 
             if (file_exists($controller)) {
                 throw new \InvalidArgumentException('That controller already exists.');
-            }else{
-                touch($controller);
             }
-            $containerAppend = file_get_contents(__DIR__ . "/../../../../templates/controller-append.tpl");
 
-            $append = file_put_contents($containerBootstrap, str_replace("{{CONTROLLER_NAME}}", $controllerName, $containerAppend), FILE_APPEND | LOCK_EX);
+            foreach ($separated as $sep) {
+                if ($sep == end($separated)) {
+                    touch($controller);
+                    $controllerFileName = $sep;
+                    continue;
+                }
+                mkdir($base . "/$sep");
+                $base = $base . "/$sep";
+                $namespace = $namespace . "\\$sep";
+            }
+            
+            $containerAppend = file_get_contents(__DIR__ . "/../../../../templates/controller-append.tpl");
+            $containerAppend = str_replace("{{CONTROLLER_NAME_STRIPPED}}", str_replace("\\", "", $controllerName), $containerAppend);
+            $containerAppend = str_replace("{{CONTROLLER_NAME}}", $controllerName, $containerAppend);
+
+            $append = file_put_contents($containerBootstrap, $containerAppend, FILE_APPEND | LOCK_EX);
 
             $phpClass = new ClassModel();
-            $phpClass->setNamespace(new NamespaceModel('Dappur\Controller'));
+            # Namespace
+            $phpClass->setNamespace(new NamespaceModel($namespace));
+
+            # Uses
+            $phpClass->addUses(new UseClassModel('Dappur\Controller\Controller as Controller'));
             $phpClass->addUses(new UseClassModel('Psr\Http\Message\ServerRequestInterface as Request'));
             $phpClass->addUses(new UseClassModel('Psr\Http\Message\ResponseInterface as Response'));
-            $name = new ClassNameModel($controllerName, 'Controller');
+
+            #Class
+            $name = new ClassNameModel($controllerFileName, 'Controller');
             $phpClass->setName($name);
-            $defaultMethod = new MethodModel('default', 'public');
+
+            # Sample Method
+            $defaultMethod = new MethodModel('sample', 'public');
             $defaultMethod->addArgument(new ArgumentModel('request', 'Request'));
             $defaultMethod->addArgument(new ArgumentModel('response', 'Response'));
-            $defaultMethod->setBody('return $this->view->render($response, \'App/home.twig\');');
+            $defaultMethod->setBody('return $this->view->render($response, \'home.twig\');');
             $phpClass->addMethod($defaultMethod);
+
+            # Render and write file
             $create = file_put_contents($controller, $phpClass->render());
 
             $output->writeln($controllerName . " controller successfully added.");
